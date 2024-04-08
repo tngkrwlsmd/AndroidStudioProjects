6a6fdfc4bd66ed04a7e7d9d12e6fd8a24eb8f03b
tngkrwlsmd <wjsdudals7590@naver.com>
2024-04-08 22:41:53 +0900

    exam4_2

---
+++ exam4_2/app/src/main/java/com/example/exam4_2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.exam4_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("직접 풀어보기 4-2");
+    }
+}
