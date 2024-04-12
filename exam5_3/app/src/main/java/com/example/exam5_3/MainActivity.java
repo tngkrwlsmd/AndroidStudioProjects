16a025e7767d69bd7a3da16aff4bdeaeddaf582c
tngkrwlsmd <wjsdudals7590@naver.com>
2024-04-12 21:18:02 +0900

    edit Project5_1, exam5_3, create exam5_4

--- exam5_3/app/src/main/java/com/example/exam5_3/MainActivity.java
+++ exam5_3/app/src/main/java/com/example/exam5_3/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.exam5_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(baseLayout, params);
+
+        EditText editText = new EditText(this);
+        Button button = new Button(this);
+        TextView textView = new TextView(this);
+
+        button.setText("버튼입니다");
+        button.setBackgroundColor(Color.rgb(255, 255, 0));
+        button.setOnClickListener(arg0 -> {
+            String text = String.valueOf(editText.getText());
+            textView.setText(text);
+        });
+
+        baseLayout.addView(editText);
+        baseLayout.addView(button);
+        baseLayout.addView(textView);
     }
 }
