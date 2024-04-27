792fa7ddbd50f9516c405ddb87dc897b6d97ab2a
tngkrwlsmd <wjsdudals7590@naver.com>
2024-04-27 21:56:45 +0900

    Project6_2

--- Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
+++ Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.project6_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUrl = findViewById(R.id.edtUrl);
+        btnGo = findViewById(R.id.btnGo);
+        btnBack = findViewById(R.id.btnBack);
+        web = findViewById(R.id.webView1);
+
+        web.setWebViewClient(new CookWebViewClient());
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+
+        btnGo.setOnClickListener(v -> web.loadUrl(edtUrl.getText().toString()));
+        btnBack.setOnClickListener(v -> web.goBack());
+    }
+    static class CookWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
     }
 }
