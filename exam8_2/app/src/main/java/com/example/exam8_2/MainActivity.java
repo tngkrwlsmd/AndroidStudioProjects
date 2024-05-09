1e0e4fcebd3ea93b32545ce4da305dcb03e36895
tngkrwlsmd <wjsdudals7590@naver.com>
2024-05-09 21:59:03 +0900

    exam8_2

---
+++ exam8_2/app/src/main/java/com/example/exam8_2/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.exam8_2;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPrev, btnNext;
+    myPictureView myPicture;
+    int curNum = 0;
+    File[] imageFiles = new File[0];
+    String imageFname;
+    TextView imageCount;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 이미지 뷰어");
+        ActivityCompat.requestPermissions(this, new String[] {
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
+        btnPrev = findViewById(R.id.btnPrev);
+        btnNext = findViewById(R.id.btnNext);
+        myPicture = findViewById(R.id.myPrictureView1);
+        imageCount = findViewById(R.id.imageCount);
+
+        File[] allFiles = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pictures").listFiles();
+        for (File allFile : Objects.requireNonNull(allFiles)) {
+            if (allFile.isFile()) {
+                imageFiles = Arrays.copyOf(imageFiles, imageFiles.length + 1);
+                imageFiles[imageFiles.length - 1] = allFile;
+            }
+            imageFname = imageFiles[curNum].toString();
+            myPicture.imagePath = imageFname;
+        }
+
+        imageCount.setText(curNum + 1 + "/" + imageFiles.length);
+
+        btnPrev.setOnClickListener(v -> {
+            if (curNum <= 0) Toast.makeText(getApplicationContext(), "첫번째 그림입니다", Toast.LENGTH_SHORT).show();
+            else {
+                curNum--;
+                imageFname = imageFiles[curNum].toString();
+                myPicture.imagePath = imageFname;
+                myPicture.invalidate();
+                imageCount.setText(curNum + 1 + "/" + imageFiles.length);
+            }
+        });
+
+        btnNext.setOnClickListener(v -> {
+            if (curNum >= imageFiles.length - 1) Toast.makeText(getApplicationContext(), "마지막 그림입니다", Toast.LENGTH_SHORT).show();
+            else {
+                curNum++;
+                imageFname = imageFiles[curNum].toString();
+                myPicture.imagePath = imageFname;
+                myPicture.invalidate();
+                imageCount.setText(curNum + 1 + "/" + imageFiles.length);
+            }
+        });
+    }
+}
---
+++ exam8_2/app/src/main/java/com/example/exam8_2/myPictureView.java
@@ -0,0 +1,46 @@
+package com.example.exam8_2;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class myPictureView extends View {
+    String imagePath = null;
+    public myPictureView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    @SuppressLint("DrawAllocation")
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (imagePath != null) {
+            @SuppressLint("DrawAllocation") Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+
+            int canvasWidth = getWidth();
+            int canvasHeight = getHeight();
+            float bitmapWidth = bitmap.getWidth();
+            float bitmapHeight = bitmap.getHeight();
+
+            float scale = Math.min(canvasWidth / bitmapWidth, canvasHeight / bitmapHeight);
+            float scaledWidth = bitmapWidth * scale;
+            float scaledHeight = bitmapHeight * scale;
+
+            float x = (canvasWidth - scaledWidth) / 2;
+            float y = (canvasHeight - scaledHeight) / 2;
+
+            canvas.drawBitmap(bitmap, null, new RectF(x, y, x + scaledWidth, y + scaledHeight), null);
+
+
+            bitmap.recycle();
+        }
+    }
+}
