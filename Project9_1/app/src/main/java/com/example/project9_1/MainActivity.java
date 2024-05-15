550fdc74b89b3095a14c357478d1c7206bdde378
tngkrwlsmd <wjsdudals7590@naver.com>
2024-05-15 15:34:56 +0900

    Project9_1

--- Project9_1/app/src/main/java/com/example/project9_1/MainActivity.java
+++ Project9_1/app/src/main/java/com/example/project9_1/MainActivity.java
@@ -1,14 +1,112 @@
 package com.example.project9_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    final static int LINE = 1, CIRCLE = 2;
+    static int curShape = LINE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        MyGraphicView mainView = new MyGraphicView(this);
+
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.setLayoutParams(new LinearLayout.LayoutParams (
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+
+        Toolbar toolbar = new Toolbar(this);
+        toolbar.setLayoutParams(new Toolbar.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        toolbar.setTitle("간단 그림판");
+        mainLayout.addView(toolbar);
+
+        mainLayout.addView(mainView, new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        setContentView(mainLayout);
+
+        setSupportActionBar(toolbar);
+    }
+
+
+    private static class MyGraphicView extends View {
+        int startX = -1, startY = -1, stopX = -1, stopY= -1;
+        public  MyGraphicView(Context context) {
+            super(context);
+        }
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    startX = (int) event.getX();
+                    startY = (int) event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                case MotionEvent.ACTION_UP:
+                    stopX = (int) event.getX();
+                    stopY = (int) event.getY();
+                    this.invalidate();
+                    break;
+            }
+            return true;
+        }
+
+        protected void onDraw(@NonNull Canvas canvas) {
+            super.onDraw(canvas);
+            @SuppressLint("DrawAllocation") Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setStrokeWidth(5);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.RED);
+
+            switch (curShape) {
+                case LINE:
+                    canvas.drawLine(startX, startY, stopX, stopY, paint);
+                    break;
+                case CIRCLE:
+                    int radius = (int) Math.sqrt(Math.pow(stopX - startX, 2) + Math.pow(stopY - startY, 2));
+                    canvas.drawCircle(startX, startY, radius, paint);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0,1,0,"선 그리기");
+        menu.add(0,2,0,"원 그리기");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                curShape = LINE;
+                return true;
+            case 2:
+                curShape = CIRCLE;
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
