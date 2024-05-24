295c45b79b03dbe6515778787f64405828738e31
tngkrwlsmd <wjsdudals7590@naver.com>
2024-05-24 14:14:21 +0900

    Project10_1

--- Project10_1/app/src/main/java/com/example/project10_1/MainActivity.java
+++ Project10_1/app/src/main/java/com/example/project10_1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.project10_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNewActivity = findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(intent);
+        });
     }
 }
